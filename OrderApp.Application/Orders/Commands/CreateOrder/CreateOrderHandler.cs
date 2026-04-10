e009a07e50441a65bc5979b89ce21ce1a314b8f9
Arif Islam <prob_sol56@hotmail.com>
2026-04-10 22:49:41 +0600

    feat: implement CreateOrderHandler skeleton for order processing

---
+++ OrderApp.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,11 @@
+using OrderApp.Application.Common.Interfaces;
+
+namespace OrderApp.Application.Orders.Commands.CreateOrder;
+
+public class CreateOrderHandler(IOrderRepository orderRepository, IUnitOfWork unitOfWork)
+{
+    public async Task<Guid> HandleAsync(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        
+    }
+}
